e66587836095a4b1bb1dcfb073514e530e650bb2
suratmasukdankeluar <rizkiyouarda@gmail.com>
2019-01-21 12:32:14 +0700

    suratmasukdankeluar
    sistem informasi surat masuk dan keluar

---
+++ views/about_tampil.php
@@ -0,0 +1,68 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <label class="col-sm-12 control-label"><center><strong><font size="5">SISTEM INFORMASI SURAT MASUK & SURAT KELUAR <br> KANTOR PELAYANAN PAJAK PRATAMA KISARAN</font></strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            <thead>
+								<tr>
+									<th>No</th>
+									<th>Nama</th>
+									<th>NIM</th>
+									<th>Kelas</th>
+									<th>No Telepon</th>
+									<th>Alamat</th>
+									<th>Email</th>
+								</tr>
+							</thead>
+							<tr>
+								<td>1</td>
+								<td>Novi Kumala</td>
+								<td>15220406</td>
+								<td>SI 7N (Source 5)</td>
+								<td>085372558556</td>
+								<td>Tanjung Alam</td>
+								<td>Novikeykumala27@gmail.com</td>
+							</tr>
+							<tr>
+								<td>2</td>
+								<td>Rizki You Arda</td>
+								<td>15220304</td>
+								<td>SI 7N (Source 5)</td>
+								<td>081276843219</td>
+								<td>Umbut-Umbut</td>
+								<td>Rizkiyouarda@gmail.com</td>
+							</tr>
+							<tr>
+								<td>3</td>
+								<td>Eci Syafitri Sirait</td>
+								<td>15220408</td>
+								<td>SI 7N (Source 5)</td>
+								<td>08235267188</td>
+								<td>Jalan Malik Ibrahim</td>
+								<td>Ecisyafitrisirait@gmail.com</td>
+							</tr>
+	
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
